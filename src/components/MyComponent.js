dfe86c8a6395efb1661da794915cd6d53290f5ea
nclight <zhumink@yonyou.com>
2018-03-08 14:03:42 +0800

    新增组件

---
+++ src/components/MyComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+export default class MyComponent extends Component {
+
+
+	render() {
+
+		return [
+				<h1 key={1}>hello world!</h1>,
+				<h1 key={2}>hello world!</h1>
+			];
+	}
+
+}
+
--- src/index.js
+++ src/index.js
@@ -1,25 +1,40 @@
 // import _ from 'lodash';
 // import React from 'react';
 // import ReactDOM from 'react-dom';
-
-
 // ReactDOM.render(
 //   <h1>Hello, world!</h1>,
 //   document.getElementById('root')
 // );
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MyAwesomeReactComponent from './components/MyAwesomeReactComponent';
+import MyComponent from './components/MyComponent';
  
 const App = () => (
   <MuiThemeProvider>
     <MyAwesomeReactComponent />
   </MuiThemeProvider>
 );
- 
+
+const Bpp = () => (
+		<MyComponent>
+			Hello, Bpp!
+		</MyComponent>
+	);
+
+function callback() {
+	console.log('execute callback');
+}
+
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <MyComponent />,
+  document.getElementById('root'),
+  callback
 );
+
+ 
+// ReactDOM.render(
+//   <App />,
+//   document.getElementById('root')
+// );
